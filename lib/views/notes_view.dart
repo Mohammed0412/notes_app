1413604f5570aa59b8d320258922d150c3135f21
Mohammed0412 <mohamedeid674mmm@gmail.com>
2026-09-10 13:18:09 +0300

    create custom search icon and create custom app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notiq/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
+
+
