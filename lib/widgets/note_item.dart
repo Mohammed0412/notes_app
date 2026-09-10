5c3fd39eb131f7911500010e0447c716911bd570
Mohammed0412 <mohamedeid674mmm@gmail.com>
2026-09-10 13:58:16 +0300

    create note item

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                "Flutter Tips",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 8),
+                child: Text(
+                  "Build Your Career With Mohammed Eid",
+                  style: TextStyle(color: Colors.black.withOpacity(.6)),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.black, size: 30),
+              ),
+            ),
+            Text(
+              "Seb10 , 2026",
+              style: TextStyle(
+                color: Colors.black.withOpacity(.6),
+                fontSize: 12,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
