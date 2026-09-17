3d0b99c35aff3f4f178f4e93c5fe7cbf6d550729
Mohammed0412 <mohamedeid674mmm@gmail.com>
2026-09-17 15:59:56 +0300

    register adapter

--- lib/cubits/add_note_cubit/add_notes_cubit.dart
+++ lib/cubits/add_note_cubit/add_notes_cubit.dart
@@ -1,7 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:notiq/models/note_model.dart';
 part "add_notes_state.dart";
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+
+  addNote(NoteModel note){}
 }
